43541c2ae1c789847c88e933c44f61c39d72a494
miguelaferreira <miguelferreira@me.com>
2015-05-18 16:12:29 +0200

    Bump version to 1.0.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mferreira@schubergphilis.com'
 license          'Apache License, Version 2.0'
 description      'Installs/Configures ratpoison window manager'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 
 supports         'centos'
 
